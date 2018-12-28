1ab4a61531a6f4392651cc92146b53bbf6098bea
greg_toombs <greg.toombs@oracle.com>
2018-12-27 20:12:44 -0500

    Add init to 1

--- 01/01.c
+++ 01/01.c
@@ -10,13 +10,13 @@ static void glfwErrorCallback(int error, const char *desc) {
     fprintf(stderr, "GLFW error 0x%08X: %s\n", error, desc);
 }
 
-int main() {
+static void init(GLFWwindow **window) {
     // Set error callback to see more detailed failure info
     glfwSetErrorCallback(glfwErrorCallback);
 
     if (!glfwInit()) {
         fprintf(stderr, "Failed to initialize GLFW\n");
-        return -1;
+        exit(1);
     }
 
     glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
@@ -27,34 +27,40 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
     // To make MacOS happy
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    // We don't want the old OpenGL 
+    // We don't want the old OpenGL
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Open a window and create its OpenGL context
-    GLFWwindow *window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
-    if (!window) {
+    *window = glfwCreateWindow(1024, 768,
+        "Tutorial 01", NULL, NULL);
+    if (!*window) {
         fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, "
                         "they are not 3.3 compatible. Try the 2.1 version of "
                         "the tutorials.\n");
         glfwTerminate();
-        return -1;
+        exit(1);
     }
+    glfwMakeContextCurrent(*window);
     
-    glfwMakeContextCurrent(window); // Initialize GLEW
     glewExperimental = true; // Needed in core profile
     if (glewInit() != GLEW_OK) {
         fprintf(stderr, "Failed to initialize GLEW\n");
         glfwTerminate();
-        return -1;
+        exit(1);
     }
 
     // Ensure we can capture the escape key being pressed below
-    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glfwSetInputMode(*window, GLFW_STICKY_KEYS, GL_TRUE);
+}
+
+int main() {
+    GLFWwindow *window;
+    init(&window);
 
     do {
-        // Clear the screen. It's not mentioned before Tutorial 02, but it can 
+        // Clear the screen. It's not mentioned before Tutorial 02, but it can
         // cause flickering, so it's there nonetheless.
         glClear(GL_COLOR_BUFFER_BIT);
 
